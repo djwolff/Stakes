ceba94909143ea50983225f056a90621b0e521ee
SLandgra <slandgra@ucsd.edu>
2017-08-16 15:05:42 -0700

    fixed styling bugs

--- components/App.js
+++ components/App.js
@@ -138,7 +138,7 @@ export default StackNavigator({
 //Styles
 const styles = StyleSheet.create({
   container: {
-    flex: '1',
+    flex: 1,
     // justifyContent: 'center',
     // alignItems: 'center',
   },
